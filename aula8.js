1f29e5d8ded370e7847d30a1529a1ec75531fd8f
Miguel <edmarllynsoaresconfessor@gmail.com>
2024-10-22 20:43:18 -0300

    EDY
    EDY

--- aula8.js
+++ aula8.js
@@ -5,5 +5,11 @@ const investimentoMensal = [ 1000, 2000, 3000, 4000, 5000, 6000];
 console.log(investimentoMensal);
 
 console.log(`O valor investido no mês de Janeiro foi:  ${investimentoMensal[4]}`);
+//ACESSO AO INDICE DO ELEMENTO ARRAY
 
-//javascript arraysed
+
+
+
+
+
+//javascript arrays
