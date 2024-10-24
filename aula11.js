6f541979c16608f8b65c1e6a316e0f2ee2b821a6
Aline Araujo <edmarllynsoaresconfessor@gmail.com>
2024-10-24 20:09:33 -0300

    ed
    ed

---
+++ aula11.js
@@ -0,0 +1,15 @@
+//ARRAY
+
+let lista = ["Banna", 23 , true,'Maça']; //Usamos tipos diferentes dentro da variavel (string, number, boolean)
+
+lista = ["Banana", "Maça", "Pêra"];
+//Por se tratar de uma variavel tipo let, conseguimos alterar as informações que estão dentro dela
+
+console.log(lista);
+
+
+lista[0] = "Laranja";// Aqui acessamos o indice 0 da lista e alteramos o valor para "Laranja"
+lista[1] = "Melancia";// Aqui acessamos o indice 0 da lista e alteramos o valor para "Melancia"
+lista[2] = "limão";// Aqui acessamos o indice 0 da lista e alteramos o valor para "Limão"
+
+console.log(lista[2]);//Podemos imprimir no terminal o valor total da variavel lista, mas tambem conseguimos ultilizar o indice para uma impressão especifica
---
+++ aula12.js
@@ -0,0 +1,12 @@
+let matrizVendas = [
+    [100, 200,300], //Loja A
+    [400, 500, 50], //Loja B
+    [700, 400, 450], //Loja C
+]
+
+console.log(matrizVendas[2]);
+
+
+matrizVendas [0][1] = 800;
+
+console.log(matrizVendas [0]);
