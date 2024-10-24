1a52606a7ccbe7bdde8cf0bfdcc6351817ab2aa5
Aline Araujo <edmarllynsoaresconfessor@gmail.com>
2024-10-24 20:56:48 -0300

    edy
    edy

--- aula12.js
+++ aula12.js
@@ -4,9 +4,22 @@ let matrizVendas = [
     [700, 400, 450], //Loja C
 ]
 
-console.log(matrizVendas[2]);
+//console.log(matrizVendas[2]);
 
 
 matrizVendas [0][1] = 800;
-
 console.log(matrizVendas [0]);
+
+matrizVendas [2] [1] = 900;
+console.log(matrizVendas[2]);
+
+matrizVendas [1] [1] = 145;
+console.log (matrizVendas[1]);
+
+let resultadoLojaA = matrizVendas[0][0] + matrizVendas[0][1] + matrizVendas [0][2];
+
+let resultadoLojaB = matrizVendas[1][0] + matrizVendas[1][1] + matrizVendas [1][2];
+
+let resultadoLojaC = matrizVendas[2][0] + matrizVendas[2][1] + matrizVendas [2][2];
+
+console.log(resultadoLojaA, resultadoLojaB, resultadoLojaC);
