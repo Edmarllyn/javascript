4be76279695bbc4ba26cc7221ec7a221bbfaccc5
Miguel <edmarllynsoaresconfessor@gmail.com>
2024-11-19 20:20:31 -0300

    ed

--- carrossel 2/script.js
+++ carrossel 2/script.js
@@ -3,14 +3,19 @@ const track = document.getElementById('carroselTrack');
 
 // Obtém o número de imagens no carrossel
 const items = document.querySelectorAll('.carousel-item');
+
+// variavel que mantém o índice da imagem atual exibida no carrossel ( começa em 0, a primeira imagem)
 let index = 0;
+
+
 
 // Função que move o carrossel para a próxima imagem
 function moveCarousel() {
-    index++;
+    //icrementa o indice em 1 para avançar para a proxima image
 
-    // Verifica se o índice chegou à última imagem
-    if (index >= items.length) {
+    index++;
+    // Verifica se o índice atual é maior ou igual a proxima imagem
+    if (index >= items.length- 1) {
         // Pequeno atraso para permitir que a transição termine
         setTimeout(() => {
             track.style.transition = 'none'; // Remove a animação
@@ -18,10 +23,10 @@ function moveCarousel() {
             track.style.transform = `translateX(0)`; // Volta para o início
         }, 500); // Atraso de 0,5 segundos
     } else {
-        track.style.transition = 'transform 0.5s ease'; // Adiciona a animação
+        track.style.transition = 'transform 0.2s ease'; // Adiciona a animação
         track.style.transform = `translateX(-${index * 100}%)`; // Desloca o carrossel
     }
 }
 
 // Define um intervalo para mover o carrossel automaticamente a cada 3 segundos
-setInterval(moveCarousel, 3000);
+setInterval(moveCarousel, 1000);
